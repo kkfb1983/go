7665c3493276a09c2c0c3f98ff861cc92bf7985b
建鑫鄂 <6503611@qq.com>
2019-12-05 14:58:01 +0800

    1205 1457

--- main.go
+++ main.go
@@ -6,11 +6,30 @@ import (
 )
 
 func main() {
-	var c int
+	/** add func */
+	//var c int
 	// c = add(3, 8)
 	param := src.Add(5, 15)
 	fmt.Println("hello word", param)
 	// go fmt.Println("hello word", param)
-	c = int(src.CTof(12.13))
+	/** tempconv func */
+	//var c src.Celsius
+	//var f src.Fahrenheit
+	//c = int(src.CTof(12.13))
+	//fmt.Println(c)
+	//fmt.Printf("%g\n",src.BoilingC-src.FreezingC)
+	//boilingF := src.CTof(src.BoilingC)
+	//fmt.Printf("%g\n",boilingF-src.CTof(src.FreezingC))
+	//fmt.Printf("%g\n",boilingF-src.FreezingC)
+	//fmt.Println(c==0)
+	//fmt.Println(f>=0)
+	////fmt.Println(c==f)
+	//fmt.Println(c == src.Celsius(f))
+	c := src.FToC(212.0)
+	fmt.Println(c.String())
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%s\n", c)
 	fmt.Println(c)
+	fmt.Printf("%g\n", c)
+	fmt.Println(float64(c))
 }
